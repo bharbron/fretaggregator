2a630c23ecafb708ab745f02e04d7d85f8674f76
Brian Harbron <harbron@gmail.com>
2015-06-21 20:24:26 +0000

    Started working on templates for site

--- fretaggregator/views.py
+++ fretaggregator/views.py
@@ -3,3 +3,12 @@ from flask import render_template
 import models
 from fretaggregator import app
 from .database import session
+
+@app.route("/", methods=["GET"])
+def home():
+  return render_template("home.html")
+
+@app.route("/results", methods=["GET"])
+def results():
+  pass
+  #return render_template("results.html")
