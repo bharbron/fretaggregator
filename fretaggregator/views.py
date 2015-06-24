55ff3576774596ef2b540acd058b3082451481d7
Brian Harbron <harbron@gmail.com>
2015-06-24 19:50:38 +0000

    Added macro for rendering a list of submissions

--- fretaggregator/views.py
+++ fretaggregator/views.py
@@ -1,12 +1,17 @@
 from flask import render_template
 
-import models
+from models import Submission
 from fretaggregator import app
 from .database import session
 
 @app.route("/", methods=["GET"])
 def home():
-  return render_template("home.html")
+  """ Main page of the site """
+  # Get a list of the most recent submissions
+  recent_submissions = session.query(Submission).order_by(Submission.submission_date.desc()).limit(3)
+  return render_template("home.html",
+                        recent_submissions=recent_submissions
+                        )
 
 @app.route("/results", methods=["GET"])
 def results():
