04318726193963c081a58a9ad355d9d2fcfb5128
Brian Harbron <harbron@gmail.com>
2015-07-14 19:26:44 +0000

    Update heroku config

--- fretaggregator/config.py
+++ fretaggregator/config.py
@@ -4,3 +4,8 @@ class DevelopmentConfig(object):
   SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/fretaggregator"
   DEBUG = True
   SECRET_KEY = os.environ.get("FRETAGGREGATOR_SECRET_KEY", "")
+  
+class HerokuConfig(object):
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgres://zcvdykrqcjbdca:7xUaDYbMf0LKCUuJ2mwPLwa32g@ec2-54-197-238-19.compute-1.amazonaws.com:5432/dbhoq2p6p7too5")
+  DEBUG = False
+  SECRET_KEY = os.environ.get("FRETAGGREGATOR_SECRET_KEY", "")
