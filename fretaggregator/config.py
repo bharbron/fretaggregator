86510239bc0e157253377e392f0ed370323604b6
Brian Harbron <harbron@gmail.com>
2015-07-21 01:15:20 +0000

    Updated development config for Cloud9 environment

--- fretaggregator/config.py
+++ fretaggregator/config.py
@@ -1,11 +1,11 @@
 import os
 
 class DevelopmentConfig(object):
-  SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/fretaggregator"
+  SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/fretaggregator"
   DEBUG = True
   SECRET_KEY = os.environ.get("FRETAGGREGATOR_SECRET_KEY", "")
   
 class HerokuConfig(object):
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgres://zcvdykrqcjbdca:7xUaDYbMf0LKCUuJ2mwPLwa32g@ec2-54-197-238-19.compute-1.amazonaws.com:5432/dbhoq2p6p7too5")
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "")
   DEBUG = False
   SECRET_KEY = os.environ.get("FRETAGGREGATOR_SECRET_KEY", "")
